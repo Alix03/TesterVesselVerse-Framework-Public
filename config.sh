4516c2d224516588439f8a4a4152ce852d6ca67f
Alix03 <alicebdf2@gmail.com>
2025-11-05 09:37:54 +0100

    Add .gitignore and .dvcignore files for improved performance and configuration management

---
+++ config.sh
@@ -0,0 +1,28 @@
+#SLICER_PATH="/home/dany/00_2025/3DSLICER/Slicer-5.6.2-linux-amd64/Slicer"
+SLICER_PATH='/Applications/Slicer.app/Contents/MacOS/Slicer'
+
+owner_auth_path='/home/dany/00_2025/MICCAI25/CREDENTIAL_DVC_VV/OWNER/vesselverse25-0b6012c4bf10.json'
+user_auth_path='/home/dany/00_2025/MICCAI25/CREDENTIAL_DVC_VV/SERVICE/vesselverse25-71763b77a34d.json'
+
+
+
+DATASET_NAME='COW23MR' # [IXI, COW23MR, COW23CT, COW24MR, COW24CT]
+database_ID='1Lt5rGwBPkmdXGeGmpNKrzZ07xJzY_yYv'
+user_upload_ID='1PoD3eV41h_EWb1uDuMNGlfIUg8gi5_8a'
+
+
+# IXI
+### DATABASE_ID: STORAGE (1Lt5rGwBPkmdXGeGmpNKrzZ07xJzY_yYv)
+#1. **BravePanda**      :    USER1         (1OzAuvCBUkH2Lt4HOBxmEPmdYR1H88C-Y)
+#2. **SwiftCheetah**    :    USER2         (1PoD3eV41h_EWb1uDuMNGlfIUg8gi5_8a)
+#3. **SilentOwl**       :    USER3         (1dgXeYwaOxKeS1NGJSBCzatk8l1Qwmh8q)
+
+
+
+# COW23_MR
+### DATABASE_ID: STORAGE (1jBDBZb8MNNXpGGWe0nTeOLlK_KIIW60p)
+
+#1. **BravePanda**      :    USER1         (1HaOSkx8ANp9S-mzSh-XJ56ZapTBsHCDX)
+#2. **SwiftCheetah**    :    USER2         (1mdgCXcgNuLtP6YkCNyWbkNCbPiG0AHzZ)
+#3. **SilentOwl**       :    USER3         (1ObMn6yrPPnw4Ua8OYFt34ct7lcakK1OU)
+
